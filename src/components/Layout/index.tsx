696935f6baf0c25051eff7450a873e5e308a7110
Dchole <derekoware47@gmail.com>
2021-05-27 19:21:44 +0000

    Fixed isAuthPage login bug

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,11 +5,11 @@ import BottomNavbar from "../BottomNavbar"
 import { useRouter } from "next/router"
 
 const Layout: React.FC = ({ children }) => {
+  const { pathname } = useRouter()
   const { breakpoints } = useTheme()
   const desktop = useMediaQuery(breakpoints.up("sm"))
-  const { pathname } = useRouter()
 
-  const isAuthPage = pathname === "/login" || "/register"
+  const isAuthPage = pathname === "/login" || pathname === "/register"
 
   return (
     <>
